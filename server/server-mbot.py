39319f83c80cf9b845eb6da4568ffe236015383b
Ausine Sieh <asiehh47@gmail.com>
2026-04-29 17:21:57 -0400

    Fixed follow line implementation

--- server/server-mbot.py
+++ server/server-mbot.py
@@ -937,55 +937,6 @@ def move_object_behavior():
     finally:
         arbiter.release("motors", "MOVE_OBJECT")
 
-
-@register_command("MOVE_OBJECT")
-def handle_move_object(payload):
-    move_object_behavior()
-    return ok_response("MOVE_OBJECT complete!")
-
-def move_object_behavior():
-
-    # Read distance to object
-    if arbiter.acquire("ultrasonic", "MOVE_OBJECT", 50):
-        try:
-            distance = mbuild.ultrasonic2.get()
-        finally:
-            arbiter.release("ultrasonic", "MOVE_OBJECT")
-
-    # If nothing detected do nun
-    if distance is None or distance > 15:
-        return
-
-    # Acquire motors before moving
-    if arbiter.acquire("motors", "MOVE_OBJECT", 50):
-        try:
-            # Turn 180 so back faces the object
-            cyberpi.console.print("Tried to turn")
-            turn(180)
-            cyberpi.console.print("Got past yield")
-
-
-            # Move backward towards the object
-            mbot2.straight(-(distance * 1.3))
-            cyberpi.console.print("hello")
-
-            # Push object sideways
-            mbot2.drive_speed(-30, 50)
-            time.sleep(1.5)
-            mbot2.drive_speed(0, 0)
-
-            # Return to the original position
-            mbot2.drive_speed(30, -50)
-            time.sleep(1.5)
-            mbot2.drive_speed(0, 0)
-
-            # Step 7: Turn back to the original direction
-            turn(180)
-
-        finally:
-            arbiter.release("motors", "MOVE_OBJECT")
-
-
 @register_command("MOVE_OBJECT")
 def handle_move_object(payload):
     move_object_behavior()
@@ -1016,6 +967,6 @@ def follow_line_behavior():
     mbot2.drive_speed(left_speed, right_speed)
 
 @register_command("FOLLOW_LINE")
-def handle_following_line():
+def handle_following_line(payload):
     scheduler.start_behavior("FOLLOW_LINE", follow_line_behavior)
     return ok_response("We're following the line!")
